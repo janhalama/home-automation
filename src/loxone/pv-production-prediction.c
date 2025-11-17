9fb2ad0d86f064da9c26d409ea5524bce33270dd
Jan Halama <mail@janhalama.cz>
2025-11-17 22:13:19 +0100

    fix: remove breaking include

--- src/loxone/pv-production-prediction.c
+++ src/loxone/pv-production-prediction.c
@@ -11,8 +11,6 @@ Outputs:
 - Output 2: PV production prediction for tomorrow
 */ 
 
-#include "../lib/forecast_solar.h"
-
 // Define all required constants
 #define SERVER_ADDRESS "api.forecast.solar"
 
